298fc88735cb449f93f2fa7c316f08313f263789
Janette Chairez <ChairezFamily@Janettes-iMac-3.local>
2020-11-12 21:59:54 -0600

    adding filter

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -20,3 +20,29 @@ var form = d3.select("form");
 
 button.on("click", filtertable);
 form.on("submit", filtertable);
+
+function filtertable(){
+    d3.event.preventDefault();
+
+    var fil = 0;
+
+    var datetime = d3.select("#datetime").property("value");
+    if(datetime.length > 0){
+        var ufo = tableData.filter(function(sighting){
+            return sighting.datetime == datetime
+        });
+        fil ++;
+    };
+
+    var city = d3.select("#city").property("value");
+    if(city.length > 0 && fil >0){
+        ufo = ufo.filter(function(sighting){
+            return sighting.city == city
+        });
+        fil++;
+    }else if(city.length >0 && fil ==0){
+        var ufo = tableData.filter(function(sighting){
+            return sighting.city == city
+        });
+        fil ++;
+    };
