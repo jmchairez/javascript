bfc11f00beaaded70882ebd283eaebf080bad86d
Janette Chairez <ChairezFamily@Janettes-iMac-3.local>
2020-11-12 21:46:28 -0600

    next section

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,3 +2,21 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tablebody = d3.select("tbody");
+
+tableData.forEach(function(sighting){
+    var new_row = tablebody.append("tr")
+    new_row.append("td").text(sighting.datetime);
+    new_row.append("td").text(sighting.city);
+    new_row.append("td").text(sighting.state);
+    new_row.append("td").text(sighting.country);
+    new_row.append("td").text(sighting.shape);
+    new_row.append("td").text(sighting.durationMinutes);
+    new_row.append("td").text(sighting.comments);
+});
+
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+
+button.on("click", filtertable);
+form.on("submit", filtertable);
